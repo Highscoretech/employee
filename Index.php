b1d5594afe40ba112038be58965db7dcf7aa8080
unknown <valiantotung1@gmail.com>
2022-03-11 06:23:58 +0100

    Added index file and composer.json

--- Index.php
+++ Index.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Rosnac form</title>
+</head>
+<body>
 <style>
 * {
   box-sizing: border-box;
@@ -248,5 +253,6 @@ input[type=submit]:hover {
   </form>
 </div>
 
+  
 </body>
 </html>
